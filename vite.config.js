c5f27ef5fe82f5d0ec09a083d9a2adf2bb509f51
SvenRadic <radic.sven@gmail.com>
2024-05-18 11:36:18 +0200

    bug fix

--- vite.config.js
+++ vite.config.js
@@ -6,16 +6,6 @@ import fs from 'fs'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port:7031,
-    proxy: {
-      '/api': {
-        target: 'https://localhost:7030', // Your ASP.NET MVC app URL
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      }
-    }
-  },
   build:{
     outDir: 'dist',
     rollupOptions:{
